b1de9ce2eabd6ed29f51489ef9b919810ea7e517
liuhuan <liuhuan@liuhuan-PC>
2017-03-05 21:09:59 +0800

    添加Tiles功能 但是不成功

---
+++ WebContent/WEB-INF/layout/page.jsp
@@ -0,0 +1,19 @@
+    <%@ taglib uri="http://www.springframework.org/tags" prefix="s" %>
+    <%@ taglib uri="http://tiles.apache.org/tags-tiles" prefix="t" %>
+    <%@ page session="false" %>
+<html>
+<head>
+<%-- <link rel="stylesheet" type="text/css"  href='<s:url value="/css/style.css"></s:url>'> --%>
+</head>
+<body>
+	<div id="header" >
+		<t:insertAttribute name="header"/>
+	</div>
+	<div id="content">
+		<t:insertAttribute name="body"/>
+	</div>
+	<div id="footer" >
+		<t:insertAttribute name="footer"/>
+	</div>
+</body>
+</html>
--- src/liuhuan/config/WebConfig.java
+++ src/liuhuan/config/WebConfig.java
@@ -1,33 +1,34 @@
 package liuhuan.config;
 
 import org.springframework.context.MessageSource;
-import org.springframework.context.MessageSourceResolvable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.validation.MessageCodesResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceView;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("liuhuan.web")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-	@Bean
+/*	@Bean
 	public ViewResolver viewResolver(){
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
 		resolver.setExposeContextBeansAsAttributes(true);
 		return resolver;
+	}*/
+	
+	@Bean
+	public ViewResolver viewResolver(){
+		return new TilesViewResolver();
 	}
 	
 	@Bean
@@ -37,7 +38,16 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		return messageSource;
 	}
 
-
+	@Bean
+	TilesConfigurer tilesConfigurer(){
+		TilesConfigurer configurer = new TilesConfigurer();
+		//指定tile的位置
+		configurer.setDefinitions("/WEB-INF/layout/tiles.xml");
+		//启用刷新功能
+		configurer.setCheckRefresh(true);
+		return configurer;
+	}
+	
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		// TODO Auto-generated method stub
